e56724e998c298a50ea5da6c7a74dbff8855b13b
徐强 <xuqiang@xuqiangdeMacBook-Pro.local>
2018-01-14 12:38:16 +0800

    版本1.0.0

---
+++ demo/router/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+/* smartImport */
+const AAA = () => import('../pages/AAA.vue')
+const pageB = () => import('../pages/pageB.vue')
+/* smartImport */
+
+Vue.use(Router)
